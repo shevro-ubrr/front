8304f897001ecac04e0c100090acf9cb80c1022e
wswfws <wswfws@ya.ru>
2025-07-09 11:56:38 +0500

    add courses

--- app/tests/page.tsx
+++ app/tests/page.tsx
@@ -1,29 +1,35 @@
 'use client'
 
-import {useState} from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 type ITest = {
     id: string;
     name: string;
-    description: string;
 }
 
+const imageSize = 96;
+
 const tests: ITest[] = [
-    {id: "35", name: "как выйти на пенсию в 35", description: "Test 1"},
-    {id: "money", name: "личный и семейный бюджет", description: "Test 2"},
-    {id: "paper", name: "ценные бумаги", description: "Test 3"},
+    {id: "35", name: "Как выйти <br/> на пенсию в 35"},
+    {id: "money", name: "Личный и семейный бюджет"},
+    {id: "paper", name: "Ценные бумаги"},
 ]
 
 export default function TestsPage() {
 
     return (
         <main className="main">
-            {tests.map((test, i) => (
-                <div key={i} className="testCard">
-                    <p>{test.name}</p>
-                    <p>{test.description}</p>
-                </div>
-            ))}
+            <div className="testCardContainer">
+                {tests.map((test, i) => (
+                    <Link key={i} className="testCard" href={`/tests/${test.id}`}>
+                        <Image src={"/logo_test_1.svg"} alt={""} width={imageSize} height={imageSize}
+                               className="absolute translate-x-44"/>
+                        <p>Пройдено</p>
+                        <p className="w-40 mt-4" dangerouslySetInnerHTML={{ __html: test.name }} />
+                    </Link>
+                ))}
+            </div>
         </main>
     )
 }
