ed65e06326821279d99fe054777146d113465b1a
wswfws <wswfws@ya.ru>
2025-07-10 14:34:53 +0500

    try fix button

--- shared/TextSelectionPopup.jsx
+++ shared/TextSelectionPopup.jsx
@@ -25,8 +25,6 @@ export default function TextSelectionPopup() {
                 });
 
                 setShowPopup(true);
-            } else {
-                setShowPopup(false);
             }
         };
 
